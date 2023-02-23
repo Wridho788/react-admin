99fbb2cb4a746c8a398f8ce48b14a98c1f8a911e
Wridho788 <wridho246@gmail.com>
2023-02-23 16:08:10 +0700

    complete dashboard admin, add line, pie, geography, etc. ready to deploy

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import Team from './layout/team';
 import Invoices from './layout/invoices';
 import Contacts from './layout/contacts';
 import Form from './layout/form';
-// import Bar from './layout/bar';
-// import Line from './layout/line';
-// import Pie from './layout/pie';
+import Bar from './layout/bar';
+import Line from './layout/line';
+import Pie from './layout/pie';
 import FAQ from './layout/faq';
-// import Geography from './layout/geography';
+import Geography from './layout/geography';
 import Calender from './layout/calender/calender';
 
 function App() {
@@ -28,20 +28,16 @@ function App() {
               <Topbar />
               <Routes>
                 <Route path='/' element={<Dashboard />} />
-                <Route path="/team" element={<Team />} />
-                <Route path="/contacts" element={<Contacts />} />
-                <Route path="/invoices" element={<Invoices />} />
-                <Route path="/form" element={<Form />} />
-                <Route path="/calender" element={<Calender />} />
-                <Route path="/faq" element={<FAQ />} />
-
-                {/* 
-                
-                <Route path="/bar" element={<Bar/>} />
-                <Route path="/pie" element={<Pie/>} />
-                <Route path="/line" element={<Line/>} />
-                <Route path="/geography" element={<Geography/>} />
-               */}
+                <Route path='/team' element={<Team />} />
+                <Route path='/contacts' element={<Contacts />} />
+                <Route path='/invoices' element={<Invoices />} />
+                <Route path='/form' element={<Form />} />
+                <Route path='/calender' element={<Calender />} />
+                <Route path='/faq' element={<FAQ />} />
+                <Route path='/bar' element={<Bar />} />
+                <Route path='/pie' element={<Pie />} />
+                <Route path='/line' element={<Line />} />
+                <Route path='/geography' element={<Geography />} />
               </Routes>
             </main>
           </div>
